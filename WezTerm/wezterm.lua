cf73e262e2840e1df4b3aa3d1b7eb59c059afe38
n0ct1s-k8sh <fco.tomas.ol@gmail.com>
2025-01-20 17:01:06 +0100

    Add Metal in GPU backend conditional for Apple support & Increase font size

--- WezTerm/wezterm.lua
+++ WezTerm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
   -- Auto choose dGPU rendering with Vulkan
-  if gpu.backend == 'Vulkan' and gpu.device_type == 'DiscreteGpu' then
+  if (gpu.backend == 'Vulkan' and gpu.device_type == 'DiscreteGpu') or gpu.backend == 'Metal' then
     config.webgpu_preferred_adapter = gpu
     config.front_end = 'WebGpu'
     break
@@ -15,7 +15,7 @@ for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
 end
 
 config.font = wezterm.font 'MesloLGS NF'
-config.font_size = 10.0
+config.font_size = 14.0
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
